35ab1db7301a07e5c641bde2a761016c9b84fd9f
kapw001 <kapw001@gmail.com>
2019-10-23 14:32:37 +0530

    Room and Rxjava Pagination Added And Working Fine

---
+++ app/src/main/java/com/pagetest/db/MyDatabase.java
@@ -0,0 +1,37 @@
+package com.pagetest.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+public abstract class MyDatabase extends RoomDatabase {
+
+    private static final String NAME = "MyDataBase";
+    private static MyDatabase INSTANCE;
+
+    public static MyDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), MyDatabase.class, NAME)
+                            // allow queries on the main thread.
+                            // Don't do this on a real app! See PersistenceBasicSample for an example.
+                            .allowMainThreadQueries()
+                            .fallbackToDestructiveMigration()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+
+    public abstract NoteDao noteDao();
+
+
+
+}
